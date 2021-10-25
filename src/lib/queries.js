0015858655024a9eb4b5efd7726304ec06efcc9b
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-10-25 22:34:21 +0200

    add sql queries to populate database

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -1,4 +1,5 @@
 import { db } from './database.js'; // import de la database
+import format from 'pg-format';
 
 //Dans ce fichier, on va regrouper toutes les requêtes faites à la base de données
 //Ces requêtes seront ensuite importées individuellement dans chaque route du répertoire API
@@ -14,7 +15,7 @@ export async function getAllUsers() {
 
 export async function createUser(user) {
   const { email, password } = user;
-  const response = await db.query(`INSERT INTO accounts (email, password) VALUES ($1, $2) RETURNING *;`, [email, password]);
+  const response = await db.query(`INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *;`, [email, password]);
   return response;
 }
 
@@ -28,4 +29,15 @@ export async function getaccounts7() {
 export async function getTotal70(account1, account2) {
   const response = await db.query(`SELECT SUM(value) FROM accounts WHERE number BETWEEN $1 AND $2;`, [account1, account2]);
   return response;
+}
+
+//import de la BG en DB
+export async function importBG(data) {
+// console.log('data:', data);
+
+  let dataToSend = [[data]];
+  let query1 = format('INSERT INTO accounts (number, label, total) VALUES %L returning *', dataToSend);
+  console.log('query1:', query1);
+  let { rows } = await db.query(query1);
+  return rows;
 }
