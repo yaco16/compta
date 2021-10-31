aaa9a8d4a4e611f9d6c04bd09d5eba42767a7d95
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-10-31 23:57:34 +0100

    upd account acontroler and model improve the query response and the error management

--- express/controllers/accountsController.js
+++ express/controllers/accountsController.js
@@ -1,22 +1,27 @@
-const Accounts = require('../models/accounts')
+const Accounts = require('../models/accounts');
 
 module.exports = {
   getAllAccounts: async (req, res) => {
-    const allAccounts = await Accounts.getAllAccounts()
+    const allAccounts = await Accounts.getAllAccounts();
     res.status(200).json(allAccounts);
   },
 
   uploadTb: async (req, res) => {
-    await Accounts.uploadTb(req.body)
-    .then(() => {
-      console.log('upload OK');
-      res.status(200).json({message: 'success'});
-    })
-    .catch(err => {
-      console.log('upload failed');
-      console.log(err);
-      res.status(400).json({message: 'error'});
-    });
+    try {
+      const response = await Accounts.uploadTb(req.body);
 
-  }
+      if (response === 'error') {
+        console.log('upload failed')
+        res.status(400).json({ message: 'error' });
+      }
+      if (response > 0) {
+        console.log('upload OK');
+        res.status(200).json({ message: 'success', data: response });
+      }
+
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ message: 'error' });
+    }
+  },
 };
--- express/models/accounts.js
+++ express/models/accounts.js
@@ -5,35 +5,59 @@ const db = require('../lib/database');
 
 class Accounts {
   static async getAllAccounts() {
-    const rows = await db.any(`SELECT * FROM accounts`);
-    return rows;
+    try {
+      const rows = await db.any(`SELECT * FROM accounts`);
+      return rows;
+
+    } catch (error) {
+      console.error(error);
+      return 'error';
+    }
   }
 
   static async uploadTb(csv) {
-    csv.shift(); // supprime la 1re ligne avec les libellés
+    try {
+      await csv.shift(); // supprime la 1re ligne avec les libellés
 
-    //conversion des datas au format de la query de pg-promise
-    //créer les variables cs et values
-    const cs = new pgp.helpers.ColumnSet(['number', 'label', 'total'], { table: 'accounts' });
+      //conversion des datas au format de la query de pg-promise
+      //créer les variables cs et values
+      const cs = new pgp.helpers.ColumnSet(['number', 'label', 'total'], { table: 'accounts' });
 
-    let values = [];
-    await csv.forEach((item) => {
-      if (item.data[0] !== '') {
-        values.push({
-          number: +item.data[0].slice(1),
-          label: item.data[1],
-          total: parseInt(item.data[2], 10),//on parse car le + devant ne permet pas de convertir en number des valeurs à zéro
+      let values = [];
+      await csv.forEach((item) => {
+        if (item.data[0] !== '') {
+          values.push({
+            number: +item.data[0].slice(1),
+            label: item.data[1],
+            total: parseInt(item.data[2], 10), //on parse car le + devant ne permet pas de convertir en number des valeurs à zéro
+          });
+        }
+      });
+
+      const query = pgp.helpers.insert(values, cs) + 'RETURNING *';
+
+      try {
+        const response = await db.tx('upload TB', async (t) => {
+          const deleteAccounts = await t.any('DELETE FROM accounts'); //on commence par effacer le contenu de la table
+          const addAccounts = await t.any(query);
+          return { deleteAccounts, addAccounts };
         });
+
+        if (response.addAccounts.length > 0) {
+          console.log('dans addaccounts');
+          return response.addAccounts.length;
+        } else {
+          return 'error';
+        }
+      } catch (error) {
+        console.log('upload failed');
+        console.error(error);
+        return 'error';
       }
-    });
-
-    const query = pgp.helpers.insert(values, cs) + 'RETURNING *';
-
-    db.task('upload TB', async (t) => {
-      const deleteAccounts = await t.any('DELETE FROM accounts');//on commence par effacer le contenu de la table
-      const addAccounts = await t.any(query);
-      return { deleteAccounts, addAccounts };
-    });
+    } catch (error) {
+      console.error(error);
+      return 'error';
+    }
   }
 }
 
