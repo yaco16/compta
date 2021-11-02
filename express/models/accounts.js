e0a0a8521585c7bf3cb997358777d2241cd33028
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-11-02 10:15:13 +0100

    add upload sales journal model

--- express/models/accounts.js
+++ express/models/accounts.js
@@ -59,6 +59,53 @@ class Accounts {
       return 'error';
     }
   }
+
+  static async uploadSalesJournal(csv) {
+    await csv.shift(); // supprime la 1re ligne avec les libellés
+
+    const cs = new pgp.helpers.ColumnSet(['journal', 'date', 'number', 'label', 'debit', 'lettering', 'credit'], { table: 'accounts' });
+
+    let values = [];
+    csv.forEach((item) => {
+      if (item.data[3] !== '') {
+        values.push({
+          journal: item.data[0],
+          date: item.data[3],
+          number: item.data[4],
+          label: item.data[5],
+          debit: parseInt(item.data[7], 10),
+          lettering: item.data[8],
+          credit: parseInt(item.data[9], 10),
+        });
+      }
+    });
+    values.pop(); //supprimer la dernière ligne vide
+
+    const query = pgp.helpers.insert(values, cs) + 'RETURNING *';
+
+      try {
+        const response = await db.tx('upload SalesJournal', async (t) => {
+          const deleteAccounts = await t.any('DELETE FROM accounts'); //on commence par effacer le contenu de la table
+          const addAccounts = await t.any(query);
+          return { deleteAccounts, addAccounts };
+        });
+
+        if (response.addAccounts.length > 0) {
+          console.log('dans addaccounts');
+          return response.addAccounts.length;
+        } else {
+          return 'error';
+        }
+      } catch (error) {
+        console.log('upload failed');
+        console.error(error);
+        return 'error';
+      }
+    } catch (error) {
+      console.error(error);
+      return 'error';
+    }
+
 }
 
 module.exports = Accounts;
