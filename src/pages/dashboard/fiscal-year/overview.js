366b08230855df90c2d660bfe4c56a066e81f095
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-11-06 02:58:06 +0100

    temp commit : before refactorisation

--- src/pages/dashboard/fiscal-year/overview.js
+++ src/pages/dashboard/fiscal-year/overview.js
@@ -1,57 +1,14 @@
-import { useState } from 'react';
-import Spinner from '../../../components/Spinner';
-import BarChart from '../../../components/Chart_bar';
+import MultiBarsChart from '../../../components/charts/MultiBars';
 
-
-const options = {
-  animation: true,
-  title: {
-    display: true,
-    text: 'Population growth (millions): Europe & Africa',
-  },
-  legend: { display: false },
-};
-
-export default function Overview({chartData}) {
-  console.log('chartData:', chartData.fy2020.labels.length);
-  const [loading, setLoading] = useState(true)
-  // chartData.fy2020.labels.length > 0 ? setLoading(false) : '';
-
-  const data = {
-    labels: chartData.fy2021.labels,
-    datasets: [
-      {
-        label: '2019/2020',
-        type: 'bar',
-        backgroundColor: '#99d98c',
-        data: chartData.fy2020.datasets[0].data,
-      },
-      {
-        label: '2020/2021',
-        type: 'bar',
-        backgroundColor: '#76c893',
-        data: chartData.fy2021.datasets[0].data,
-        fill: false,
-      },
-      {
-        label: '2021/2022',
-        type: 'bar',
-        backgroundColor: '#52b69a',
-        data: chartData.fy2022.datasets[0].data,
-        fill: false,
-      },
-    ],
-  };
+export default function Overview({ chartData }) {
 
   return (
     <>
-    <h1>Comparatif</h1>
-    {!loading ? <BarChart chartData={data} options={options} /> : <Spinner />
-       }
-
+      <h1>Comparatif</h1>
+      <MultiBarsChart chartData={chartData} chartTitle={'tableau'} />
     </>
   );
-};
+}
 
 export async function getServerSideProps() {
   const data2020 = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + 'post-monthly-turnover', {
@@ -87,11 +44,11 @@ export async function getServerSideProps() {
     }),
   });
 
-
   const chartData = {
     fy2020: await data2020.json(),
     fy2021: await data2021.json(),
-    fy2022: await data2022.json()};
+    fy2022: await data2022.json(),
+  };
 
   return {
     props: { chartData },
