82cd09ae9e9425089dd19662e3b3d1cdf189ba79
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-11-25 00:04:09 +0100

    add middleware to access dashboard

---
+++ src/pages/dashboard/_middleware.js
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+
+export function middleware(req, event) {
+  let token;
+  let userDetails;
+  const secret = process.env.ACCESS_TOKEN_SECRET;
+
+  if (req.cookies.access_token) {
+    token = req.cookies.access_token;
+    userDetails = jwt.decode(token, secret, 'HS256')
+  }
+
+  if (userDetails && userDetails.isConnected) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect('/users/login');
+  }
+}
+
